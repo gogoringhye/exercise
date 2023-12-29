ee18fe8355f392f13b4af725156730673214e8d1
gogoringhye <145514996+gogoringhye@users.noreply.github.com>
2023-12-29 17:20:40 +0900

    Add files via upload

---
+++ javaScript/js_example/29airplane/js/airplan.js
@@ -0,0 +1,2 @@
+let airplane=document.querySelector('.airplane');//airplane-scroll-timeline--img
+let airplaneScrollTimeline=document.querySelector('.airplane-scroll-timeline')//airplane-scroll-timeline
